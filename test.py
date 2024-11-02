731919cb4d1efb22b7413c066ca0051e14fdc060
cherry1818 <129907637+cherry1818@users.noreply.github.com>
2024-11-02 13:36:42 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ from sklearn.cluster import KMeans as SKLearnKMeans
 from main import kmeans
 
 # Funkcja obliczająca dokładność, porównując klastery z prawdziwymi etykietami
+#Oblicza dokładność klastrowania, porównując przypisania klastrów do rzeczywistych etykiet. 
+#Dla każdego klastra znajduje najczęściej występującą rzeczywistą etykietę i zlicza, 
+#ile punktów w tym klastrze ma tę samą etykietę. Całkowita liczba poprawnych przypisań jest
+#dzielona przez całkowitą liczbę punktów danych, aby uzyskać dokładność.
 def calculate_accuracy(clusters, true_labels, k):
     correct = 0
     for i in range(k):
